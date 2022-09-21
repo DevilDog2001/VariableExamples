1a99339de333bc87ecea033a2708bf74c2a3a1a8
Richardson Antoine <91235784+DevilDog2001@users.noreply.github.com>
2022-09-21 19:26:31 -0400

    Add files via upload

---
+++ Ex3c_variable_names_incorrect.py
@@ -0,0 +1,14 @@
+# Chap2 - Variables
+# Incorrect variable names
+
+p = "Python"       # this is ok, but not descriptive, and therefore not recommended
+print(p)
+
+Class = "Python"    # a variable name cannot contain keywords
+print(Class)
+
+my_course = "Python"  # a variable name cannot contain spaces
+print(my_course)
+
+course_123 = "Python"  # a variable name cannot start with a number
+print(course_123)
